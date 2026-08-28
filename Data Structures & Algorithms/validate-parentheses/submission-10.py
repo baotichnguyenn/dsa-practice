d63139cb8d05883463b2c307b7d5fc1e59617c73
Tich Bao Nguyen <nguyentichbao@gmail.com>
2026-08-28 21:05:58 +1000

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        matching_close_and_open = {
+            "[": "]",
+            "{": "}",
+            "(": ")"
+        }
+
+        opening_stack =[]
+        for char in s:
+            if char in matching_close_and_open.keys():
+                opening_stack.append(char)
+            else:
+                if len(opening_stack) ==0:
+                    return False
+                if char != matching_close_and_open[opening_stack.pop()]:
+                    return False
+        if len(opening_stack) != 0:
+            return False
+        else:
+            return True
+            
+
+           
+
+            
+        
